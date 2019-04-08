9640658322c194fb85f3cae86917063ca900da23
morswin22 <xmorswinx@gmail.com>
2019-04-08 14:49:32 +0200

    Added dashboard and post adding

--- api/index.php
+++ api/index.php
@@ -10,6 +10,12 @@ $setupPath = "data/setup.json";
 $postsPath = "data/posts.json";
 $imagesPath = "data/images/";
 
+// quickly deliver posts
+if (isMethod('get')) {
+    // check all things
+    exit();
+}
+
 // setup users database
 if (!file_exists($usersPath) || count(loadJSON($usersPath)) == 0) {
     if(isMethod("addUser") && isPosted('name', 'email', 'pass')) {
@@ -22,6 +28,12 @@ if (!file_exists($usersPath) || count(loadJSON($usersPath)) == 0) {
     }
 }
 $users = loadJSON($usersPath);
+
+// check if user wants to logout 
+if (isMethod('logout')) {
+    unset($_SESSION['user']);
+    redirect($apiPath);
+}
 
 // check if user is logged
 if (!isLogged()) {
@@ -66,8 +78,46 @@ if (isMethod('info')) {
     print_r($setup);
     print_r($posts);
     echo "</pre>";
+} else if (isMethod("settings")) {
+
+} else if (isMethod("add")) {
+    $hasAll = true;
+    foreach ($setup as $requirement) {
+        if (!isPosted($requirement['id'])) {
+            $hasAll = false;
+        }
+    }
+    if ($hasAll) {
+        $replacements = array(
+            "POST_ID" => count($posts)+1, // because of the indexing
+            "CURRENT_TIME&DATE" => date("d.m.Y H:i"),
+            "CURRENT_DATE" => date("d.m.Y"),
+            "CURRENT_TIME" => date("H:i"),
+            "CURRENT_USER" => $user['name'],
+        );
+        $newPost = array();
+        foreach ($setup as $requirement) {
+            $value = $_POST[$requirement['id']]; // sanitaze this pls
+            foreach ($replacements as $find => $replace) {
+                $value = preg_replace("/$find/m", $replace, $value);
+            }
+            $newPost[$requirement['id']] = $value;
+        }
+        unset($user['pass']); // clear that
+        $posts[] = array(
+            "data" => $newPost,
+            "creator" => $user,
+            "date" => date("d.m.Y"),
+            "time" => date("H:i"),
+            "postID" => $replacements["POST_ID"]
+        );
+        saveJSON($postsPath, $posts);
+        redirect($apiPath);
+    } else {
+        render("add", array("format"=>$setup, "url"=>$apiPath));
+    }
 } else {
-    render('dashboard', array('userName'=>$user['name']));
+    render('dashboard', array('userName'=>$user['name'], 'posts'=>$posts));
 }
 
 ?>
